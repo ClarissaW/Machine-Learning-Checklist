b25082b8b5ae08466c7b831a22c177f650df651a
Zewen Wang <w.zewen@outlook.com>
2018-06-28 19:54:06 -0400

    Preprocess

---
+++ normalization_downcast.py
@@ -0,0 +1,9 @@
+import nltk
+
+text = "This is Andrew's text, isn't it?"
+
+capitalized_text = text[0].lower() + text[1:]
+print(capitalized_text)
+
+
+
--- stemming_lemmatization.py
+++ stemming_lemmatization.py
@@ -9,5 +9,5 @@ tokens = tokenizer.tokenize(text)
 stemmer = nltk.stem.PorterStemmer()
 print(''.join(stemmer.stem(token) for token in tokens))
 
-stemmer = nltk.stem.WordNetLemmatizer()
-print(''.join(stemmer.lemmatize(token) for token in tokens))
+lemmatizer = nltk.stem.WordNetLemmatizer()
+print(''.join(lemmatizer.lemmatize(token) for token in tokens))
